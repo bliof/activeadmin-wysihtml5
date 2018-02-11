7cc02c76fe89806e8c4babebe5724e4e854bbdfc
Aleksandar Ivanov <aivanov92@gmail.com>
2018-02-11 22:56:10 +0200

    Drop some dead code related to requiring of assets

--- lib/active_admin/wysihtml5/rails/engine.rb
+++ lib/active_admin/wysihtml5/rails/engine.rb
@@ -2,22 +2,15 @@ module ActiveAdmin
   module Wysihtml5
     module Rails
       class Engine < ::Rails::Engine
-        engine_name "activeadmin_wysihtml5"
+        engine_name 'activeadmin_wysihtml5'
 
-        initializer "precompile hook", group: :all do |app|
+        initializer 'precompile hook', group: :all do |app|
           app.config.assets.precompile += [
-            "activeadmin-wysihtml5/base.js",
-            "activeadmin-wysihtml5/base.css",
-            "activeadmin-wysihtml5/wysiwyg.css"
+            'activeadmin-wysihtml5/base.js',
+            'activeadmin-wysihtml5/base.css',
+            'activeadmin-wysihtml5/wysiwyg.css'
           ]
         end
-
-        initializer "register resource" do
-          ActiveAdmin.application.load_paths += [ File.expand_path('../admin', File.dirname(__FILE__)) ]
-          ActiveAdmin.application.register_stylesheet "activeadmin-wysihtml5/base.css", :media => :screen
-          ActiveAdmin.application.register_javascript "activeadmin-wysihtml5/base.js"
-        end
-
       end
     end
   end
